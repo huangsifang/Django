2a23670fa523cfceee80f9c0cb571f25607d274b
huangsifang <17816869682@163.com>
2017-10-21 21:42:53 +0800

    cookie session 认证系统

--- guest/sign/views.py
+++ guest/sign/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -11,13 +13,21 @@ def login_action(request):
 	if request.method == 'POST':
 		username = request.POST.get('username', '')
 		password = request.POST.get('password', '')
-		if username == 'admin' and password == 'admin123':
-			return HttpResponseRedirect('/event_manage/')
+		user = auth.authenticate(username=username, password=password)
+		if user is not None:
+			auth.login(request, user) # 登录
+			response = HttpResponseRedirect('/event_manage/')
+			# response.set_cookie('user', username, 3600) # 添加浏览器cookie
+			request.session['user']=username # 将session信息记录到浏览器
+			return response
 		else:
 			return render(request,'index.html', {'error': 'username or password error!'})
 	else:
 		return render(request,'index.html', {'error': 'username or password error!'})
 
 # 发布会管理
+@login_required
 def event_manage(request):
-	return render(request,"event_manage.html")
+	# username = request.COOKIES.get('user', '') # 读取浏览器cookie
+	username = request.session.get('user', '') # 读取浏览器session
+	return render(request, "event_manage.html", {"user": username})
